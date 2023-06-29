08b38ea9110352baea80f304eab77316e4cc090f
Luiz Fernando <404.luiz@gmail.com>
2023-06-29 09:16:03 -0300

    added databinding

--- app/src/main/java/ufscar/mobile/aa1_mobile/MainActivity.java
+++ app/src/main/java/ufscar/mobile/aa1_mobile/MainActivity.java
@@ -1,12 +1,11 @@
 package ufscar.mobile.aa1_mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.view.View;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.os.Bundle;
+import ufscar.mobile.aa1_mobile.databinding.ActivityMainBinding;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,13 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.profissionalButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Criar uma intenção para iniciar a segunda Activity
-                Intent intent = new Intent(MainActivity.this, Agendamento.class);
-                startActivity(intent);
-            }
+        button.setOnClickListener(v -> {
+            // Criar uma intenção para iniciar a segunda Activity
+            Intent intent = new Intent(MainActivity.this, Agendamento.class);
+            startActivity(intent);
         });
         ImageView headerImage = findViewById(R.id.header_image);
     }
