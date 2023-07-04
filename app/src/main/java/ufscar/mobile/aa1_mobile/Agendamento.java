c9de3315adf16f595ac6436b49a82eabbac6daa5
Luiz Fernando <404.luiz@gmail.com>
2023-07-03 21:44:01 -0300

    Intent para tela de Agendamento

--- app/src/main/java/ufscar/mobile/aa1_mobile/Agendamento.java
+++ app/src/main/java/ufscar/mobile/aa1_mobile/Agendamento.java
@@ -1,13 +1,20 @@
 package ufscar.mobile.aa1_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.os.Bundle;
 public class Agendamento extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agendamento);
-
-        ImageView headerImage = findViewById(R.id.header_image);
+        // Configurar o botão de voltar
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(v -> {
+            // Encerrar a atividade atual e voltar para a atividade anterior
+            finish();
+        });
     }
 }
